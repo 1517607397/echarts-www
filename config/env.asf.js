ed4ef46bcfe17ab3f9ee2cf23e6a7879d5d083e6
自由如风 <45745414+1517607397@users.noreply.github.com>
2023-02-13 17:17:30 +0800

    Update env.asf.js

--- config/env.asf.js
+++ config/env.asf.js
@@ -1,23 +1,23 @@
 const path = require('path');
 const config = require('./common');
 
-config.host = 'https://echarts.apache.org';
+config.host = 'https://echarts.tencent.codeon.cn';
 
 config.cdnPayRootMap = {
     // Expensive!!! use it carefully.
     // zh: 'https://echarts-www.cdn.bcebos.com', // origin: 'https://echarts-www.bj.bcebos.com'
     // zh: 'https://fastly.jsdelivr.net/gh/apache/echarts-website@asf-site',
-    zh: 'https://echarts.apache.org',
-    en: 'https://echarts.apache.org'
+    zh: 'https://echarts.tencent.codeon.cn',
+    en: 'https://echarts.tencent.codeon.cn'
 };
 config.cdnFreeRootMap = {
     // 'echarts.cdn.apache.org' have been configured for zh (?)
     // zh: 'https://echarts.cdn.apache.org',
     // zh: 'https://fastly.jsdelivr.net/gh/apache/echarts-website@asf-site',
-    zh: 'https://echarts.apache.org',
-    en: 'https://echarts.apache.org'
+    zh: 'https://echarts.tencent.codeon.cn',
+    en: 'https://echarts.tencent.codeon.cn'
 };
-config.galleryPath = 'https://echarts.apache.org/examples/';
+config.galleryPath = 'https://echarts.tencent.codeon.cn/examples/';
 config.releaseDestDir = path.resolve(__dirname, '../../echarts-website');
 
 module.exports = config;
